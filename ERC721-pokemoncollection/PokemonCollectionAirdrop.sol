6effab78373aaa3a5961e432df6edbbc83f49f0d
Diego Escalona <diegoescalonarodriguez@gmail.com>
2022-11-24 22:08:12 +0100

    Upgraded ERC721Airdrop to new openzeppelin release: 4.8.0

--- ERC721-pokemoncollection/PokemonCollectionAirdrop.sol
+++ ERC721-pokemoncollection/PokemonCollectionAirdrop.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -55,15 +55,14 @@ contract PokemonCollectionAirdrop is ERC721, ERC721Enumerable, Ownable {
 
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         override(ERC721, ERC721Enumerable)
     {
-        super._beforeTokenTransfer(from, to, tokenId);
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool){
         return super.supportsInterface(interfaceId);
-    }
-
+    }  
 }
