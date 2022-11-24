e50e9edb1b1715bbf96d78f070836c0603654cd7
Diego Escalona <diegoescalonarodriguez@gmail.com>
2022-11-24 22:04:59 +0100

    Upgraded ERC721 to new openzeppelin release: 4.8.0

--- ERC721-pokemoncollection/PokemonCollection.sol
+++ ERC721-pokemoncollection/PokemonCollection.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -32,11 +32,11 @@ contract PokemonCollection is ERC721, ERC721Enumerable, ERC721URIStorage, Ownabl
         super._burn(tokenId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         override(ERC721, ERC721Enumerable)
     {
-        super._beforeTokenTransfer(from, to, tokenId);
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool){
